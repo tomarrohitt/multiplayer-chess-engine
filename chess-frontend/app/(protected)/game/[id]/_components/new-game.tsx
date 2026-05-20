4d1cc56d4ff4c896613e3ca120773b82c35f2fc0
Rohit Tomar <tomarrohit5034@gmail.com>
2026-05-20 18:48:16 +0530

    Debug: async search modal

--- chess-frontend/app/(protected)/game/[id]/_components/new-game.tsx
+++ chess-frontend/app/(protected)/game/[id]/_components/new-game.tsx
@@ -11,8 +11,8 @@ const NewGameComponent = ({ timeControl }: { timeControl: string }) => {
   return (
     <button
       onClick={() => {
-        if (timeControl) joinQueue(timeControl);
         action.setQueue(QueueStatus.WAITING, timeControl);
+        if (timeControl) joinQueue(timeControl);
       }}
       className="flex-1 px-2 py-1.5 font-mono text-xs text-emerald-400/80 bg-emerald-950/20 border border-emerald-900/30 rounded-lg hover:bg-emerald-950/40 hover:text-emerald-300 hover:border-emerald-800/50 transition-all "
     >
